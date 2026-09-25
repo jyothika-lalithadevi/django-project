438af625ced452d71aee708ee8e5d263793801ef
Jyothika Lalithadevi Dasari <jyothikalalithadevidasari@gmail.com>
2026-09-25 13:08:21 +0530

    Update views.py

--- Student/views.py
+++ Student/views.py
@@ -1,1 +1,11 @@
-
+from django.shortcuts import render
+def student(request):
+    return render(request,
+        'student.html',
+        {
+            "name":"lalitha",
+            "age":19,
+            "course":"python fsd",
+            "college":"ists",
+        }
+        )
